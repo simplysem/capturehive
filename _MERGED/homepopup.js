f6e5175f0a319e1555dd50c0a2c5243b97581eb8
fpb105 <121581437+fpb105@users.noreply.github.com>
2024-02-27 12:45:20 +0000

    Fixed popup!

--- _MERGED/homepopup.js
+++ _MERGED/homepopup.js
@@ -1,13 +1,3 @@
-// Check if "hasRead" is null (indicating it hasn't been initialized yet)
-if (hasRead === null) {
-    // Set "hasRead" to false in session storage
-    sessionStorage.setItem("hasRead", false);
-}
-
-// Retrieve the value of "hasRead" from session storage
-let hasRead = sessionStorage.getItem("hasRead");
-
-
 // Function to close the popup
 // Called out side Domload so that it can be accseed gloably
 function closePopup() {
@@ -15,13 +5,23 @@ function closePopup() {
     var popup = document.getElementById('mypopup');
     popup.style.display = 'none';
     blur.classList.remove('active'); // Remove blur class from body
-        
+            
     // Set "hasRead" to true in session storage
     // I am assuming that if you close the pop up you have read it
     sessionStorage.setItem("hasRead", 'true');
 }
 
 document.addEventListener("DOMContentLoaded", function() {
+
+    
+    // Retrieve the value of "hasRead" from session storage
+    let hasRead = sessionStorage.getItem("hasRead");
+
+    // Check if "hasRead" is null (indicating it hasn't been initialized yet)
+    if (hasRead === null) {
+        // Set "hasRead" to false in session storage
+        sessionStorage.setItem("hasRead", false);
+    }
 
     console.log("china");
 
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     // Check if "hasRead" is true (assuming you stored boolean values)
-    if (hasRead === 'false') {
+    if (hasRead === null || hasRead === 'false') {
         console.log("In the place")
 
         window.onload = function () {
